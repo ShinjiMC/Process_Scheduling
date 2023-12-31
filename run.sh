9f721ea754be2b6d704e9f8f4ba031056a8a946c
ShinjiMC <nayapbrau@gmail.com>
2023-12-31 18:41:37 -0500

    Adding run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Ejecuta el comando make para compilar
-make
+# Crear entorno virtual myenv
+python3 -m venv myenv
 
-# Comprueba si la compilación fue exitosa antes de ejecutar el programa
-if [ $? -eq 0 ]; then
-    # Ejecuta el programa generado (main)
+# Activar entorno virtual y instalar matplotlib
+source myenv/bin/activate
+pip install matplotlib
+
+# Ejecutar Makefile
+if make; then
+    # Ejecutar el programa generado (main)
     ./bin/main
 else
     echo "Error al compilar. Por favor, revisa los errores."
 fi
+
+# Ejecutar graph.py con el entorno virtual activado
+python graph.py
+
+# Desactivar entorno virtual
+deactivate
